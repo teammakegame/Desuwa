1348786d7640488e31280c2392f1420ce99b9594
lhtrung <lhtrung11@gmail.com>
2023-04-13 12:04:25 +0700

    begin

--- index.ts
+++ index.ts
@@ -1,2 +1,22 @@
-const {Client, IntentsBitField} = require('discord.js')
+import {Client, IntentsBitField} from 'discord.js';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent
+    ]
+})
+
+client.on('ready', (c) => {
+    console.log(`${c.user.username} is online.`)
+});
+
+client.on('messageCreate', (message) => {
+    console.log(message.content);
+})
+
+client.login(process.env.BOT_TOKEN);
