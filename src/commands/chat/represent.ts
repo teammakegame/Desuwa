62c9b1951f97dc6e32de4358a18f4f14c96a7d67
lhtrung <lhtrung11@gmail.com>
2023-06-03 17:18:17 +0700

    updating represent command

--- src/commands/chat/represent.ts
+++ src/commands/chat/represent.ts
@@ -5,14 +5,34 @@ export default {
         .setName("represent")
         .setDescription("Represents someone to interact")
         .addStringOption((option) => option
-            .setName("input")
-            .setDescription("Desuwa is going to represent this input")
-            .setRequired(true)
+            .setName('mode')
+            .setDescription('Desuwa will automatically represent your messages based on this option')
+            .addChoices({name: 'ON', value: 'ON'})
+            .addChoices({name: 'OFF', value: 'OFF'})
+        )
+        .addStringOption((option) => option
+            .setName("message")
+            .setDescription("Desuwa is going to represent this message instantly")
         )
         .setDMPermission(false),
     run: async (client: Client, interaction: ChatInputCommandInteraction) => {
         if (!interaction.inCachedGuild()) return;
-        interaction.channel?.send(`${interaction.options.get("input")?.value}`);
-        interaction.followUp({content: 'Representing successfully'});
+        const mode = interaction.options.get('mode')?.value;
+        const message = interaction.options.get('message')?.value;
+        console.log(`mode: ${mode}, message: ${message}`);
+        
+        switch (mode) {
+            case 'ON': 
+                await interaction.followUp({content: 'Auto representation mode is ON'})
+                break;
+            case 'OFF':
+                await interaction.followUp({content: 'Auto representation mode is OFF'})
+                break;
+            default:
+                await interaction.followUp({content: 'Complete representation'})
+                break;
+        }
+        if (message)
+            interaction.channel?.send(`${message}`);
     }
 }
