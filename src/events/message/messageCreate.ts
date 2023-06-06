07a0e248bf6dd4165a56596ea641550b7a80a7a6
lhtrung <lhtrung11@gmail.com>
2023-06-06 17:34:04 +0700

    continue build representedMessage

--- src/events/message/messageCreate.ts
+++ src/events/message/messageCreate.ts
@@ -1,5 +1,5 @@
 import client from '../../index';
-import {Events, Message } from 'discord.js';
+import {Events, Message, MessageOptions, MessagePayload, MessageType} from 'discord.js';
 import Member from '../../connections/database/models/member';
 
 export default {
@@ -7,10 +7,17 @@ export default {
     execute: async (message: Message) => {
         if (message.author.bot) return;
         console.log(message);
-        await Member.findOne({memberId: message.author.id, guildId: message.guildId})
-            .then((member: any) => {
-                if (member.get('represented') == 1) {
-                }
-            });
+    
+        const represented: boolean = !! await Member
+            .findOne({memberId: message.author.id, guildId: message.guildId})
+            .select('represented');
+        if (!represented) return;
+
+        const channel = message.channel;
+        const representedMessage = (await import(`../../messages/representedMessage`)).default(message);
+        console.log(representedMessage);
+        message.delete();
+        
+        (await channel.send(representedMessage));
     }
 }
--- src/messages/representedMessage.ts
+++ src/messages/representedMessage.ts
@@ -1,5 +1,12 @@
-import {MessageComponentBuilder} from 'discord.js';
+import {Message, MessageType, MessageOptions, MessagePayload} from 'discord.js';
 
-export default {
-    
+export default (message: Message): MessagePayload => {
+    let options: MessageOptions = {content: message.content};
+    switch (message.type) {
+        case MessageType.Default:
+            break;
+        default:
+            break;
+    }
+    return new MessagePayload(message, options);
 }
